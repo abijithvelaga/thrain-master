0e7671978bd377ecb1727f24cc20cf4b48db5a85
Velaga Abijith <abijithvelaga@gmail.com>
2023-04-26 18:02:09 -0500

    download filename change

--- src/web-application/app.py
+++ src/web-application/app.py
@@ -76,10 +76,9 @@ def download_public_key(username):
 
 @app.route('/file-directory/retrieve/file/<filename>')
 def download_file(filename):
-	print(filename)
 	filepath = UPLOAD_FOLDER+filename
 	if(os.path.isfile(filepath)):
-		return send_file(filepath, attachment_filename=filename,as_attachment=True)
+		return send_file(filepath, attachment_filename='fileMessage-thrainSecurity.txt',as_attachment=True)
 	else:
 		return render_template('file-list.html',msg='An issue encountered, our team is working on that')
 
